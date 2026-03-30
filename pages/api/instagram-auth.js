278c853962dbebfb1f6af4c67e2d0b50b7a52875
Max Anderwurzel <max5634@web.de>
2026-03-30 18:44:32 +0200

    Fix scope encoding in instagram auth

--- pages/api/instagram-auth.js
+++ pages/api/instagram-auth.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const appId = process.env.META_APP_ID
   const redirectUri = 'https://content-dashboard-snowy.vercel.app/api/instagram-callback'
-  const scope = 'instagram_basic,instagram_manage_insights,pages_show_list,pages_read_engagement'
+  const scope = encodeURIComponent('instagram_basic,instagram_manage_insights,pages_show_list,pages_read_engagement')
 
   const authUrl = 'https://www.facebook.com/v19.0/dialog/oauth' +
     '?client_id=' + appId +
